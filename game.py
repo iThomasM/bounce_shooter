aa2354ebc171aeca2ccca8b8d4dddbd92dd6c9f1
iThomasM <138166355+iThomasM@users.noreply.github.com>
2025-03-22 20:03:53 +0100

    Update game.py

--- game.py
+++ game.py
@@ -2,15 +2,16 @@ import pygame
 from player import Player
 from bullet import Bullet
 from enemies import Enemy
+from config import Config
 
 class Main:
     def __init__(self):
         pygame.init()
+        self.config = Config()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((500, 500))
+        self.screen = pygame.display.set_mode((self.config.screen_width, self.config.screen_height))
         self.running = True
         self.player = Player(self)
-        self.max_enemies = 10
         self.bullets = pygame.sprite.Group()
         self.enemies = pygame.sprite.Group()
         self.sprites = pygame.sprite.Group()
@@ -19,7 +20,7 @@ class Main:
 
     def run_game(self):
         while self.running:
-            self._enemy()
+            self._enemy_fleet()
             self._del_bullet()
             self._check_events()
             self._screen()
@@ -28,30 +29,19 @@ class Main:
             self.bullets.update()
             self.player.update()
             self.clock.tick(60)
-
+        
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 self._shoot()
+                self.player.shooting = True
             elif event.type == pygame.KEYDOWN:
                 self._check_keydown(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup(event)
     
-    def _shoot(self):
-        bullet = Bullet(self)
-        self.bullets.add(bullet)
-
-    def _del_bullet(self):
-        ...
-    
-    def _enemy(self):
-        enemy = Enemy(self, self.player)
-        if len(self.enemies.sprites()) < self.max_enemies:
-            self.enemies.add(enemy)
-
     def _check_keydown(self, event):
         if event.key == pygame.K_a:
             self.player.moving_left = True
@@ -61,6 +51,10 @@ class Main:
             self.player.moving_up = True
         elif event.key == pygame.K_s:
             self.player.moving_down = True
+        elif event.key == pygame.K_LSHIFT:
+            self.player.sprinting = True
+        elif event.key == pygame.K_SPACE:
+            self.player.dashing = True
 
     def _check_keyup(self, event):
         if event.key == pygame.K_a:
@@ -71,16 +65,53 @@ class Main:
             self.player.moving_up = False
         elif event.key == pygame.K_s:
             self.player.moving_down = False
+        elif event.key == pygame.K_LSHIFT:
+            self.player.sprinting = False
+
+    def _shoot(self):
+        bullet = Bullet(self)
+        bullet2 = Bullet(self, bul_type=2)
+        self.bullets.add(bullet)
+        self.bullets.add(bullet2)
+        self.player.bulletx = bullet.x_dis
+        self.player.bullety = bullet.y_dis
+
+    def _del_bullet(self):
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom < 0:
+                self.bullets.remove(bullet)
+            elif bullet.rect.top > 500:
+                self.bullets.remove(bullet)
+            elif bullet.rect.left < 0:
+                self.bullets.remove(bullet)
+            elif bullet.rect.right > 500:
+                self.bullets.remove(bullet)
+        pygame.sprite.groupcollide(self.bullets, self.enemies, True, True)
+
+    def _enemy_fleet(self):
+        enemy = Enemy(self, self.player)
+        if len(self.enemies.sprites()) < self.config.max_enemies:
+            self.enemies.add(enemy)
 
     def _screen(self):
-        self.screen.fill((255, 255, 255))
+        self.bg = pygame.image.load("bg.bmp")
+        self.bg = pygame.transform.scale(self.bg, (500, 500))
+        self.screen.blit(self.bg, (0, 0))
         self.player.draw_player()
-        for bullet in self.bullets.sprites():
-            bullet.draw_bullet()
-        for enemy in self.enemies.sprites():
-            enemy.spawn()
+        self._render_bullets()
+        self._render_enemies()
         pygame.display.flip()
         pygame.display.update()
+    
+    def _render_bullets(self):
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+    
+    def _render_enemies(self):
+        for enemy in self.enemies.sprites():
+            enemy.spawn()
+            if not enemy.get_distance():
+                self.enemies.remove(enemy)
 
 if __name__ == "__main__":
     game = Main()
